49e728b8956373851e0ea8293f1544dfd4bca6d0
Geree <smithzero94@gmail.com>
2023-08-26 14:21:36 +0200

    few examples in the main method

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,12 +1,11 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.xml.transform.Result;
+import java.sql.*;
 
 public class ConnectionFactory {
 
-    public static final String url = "jdbc:mysql://localhost:3306/firstdatabase";
-    public static final String user = "root";
-    public static final String password = "A7K7K7V7A7L+mysql";
+    public static final String url = System.getenv("url");
+    public static final String user = System.getenv("user");
+    public static final String password = System.getenv("password"); // System.getenv("password");
 
     public static Connection getConnection() {
         try {
@@ -17,8 +16,44 @@ public class ConnectionFactory {
         }
     }
 
-    public static void main(String[] args) {
-        getConnection();
+    public static void main(String[] args) throws SQLException {
+
+        try (Statement statement = getConnection().createStatement()){
+            String query = "SELECT * FROM user";
+            try (ResultSet resultSet = statement.executeQuery(query)){
+                while (resultSet.next()) {
+                    int id = resultSet.getInt(1);
+                    String name = resultSet.getString(2);
+                    String password = resultSet.getString(3);
+                    int age = resultSet.getInt(4);
+
+                    User user1 = new User(id, name, password, age);
+
+                    System.out.println("ID: " + id
+                                        + ", name: " + name
+                                        + ", password: " + password
+                                        + ", age: " + age);
+
+                    System.out.println(user1);
+                }
+            }
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        String preparedQuery = "SELECT name FROM user";
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(preparedQuery)) {
+            try (ResultSet resultSet = preparedStatement.executeQuery()){
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString(1));
+                }
+            }
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
